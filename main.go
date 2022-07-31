0f1b57ebfd96d8bbf35daf81c7139cd888c94706
moroishi-akira <a.moroishi@cretivesurvey.com>
2022-07-31 16:52:40 +0900

    module を取り入れる練習

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"rsc.io/quote"
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello world!!, %s!", request.URL.Path[1:])
+	fmt.Fprintf(writer, "%s", quote.Go())
 }
 
 func main() {
